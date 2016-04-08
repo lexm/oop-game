7308dac7b0d7c03d35ce4eb3b2086716675915f9
Lex Myers <lex.myers@gmail.com>
2016-04-07 18:29:37 -0700

    now creates player character

--- entry.js
+++ entry.js
@@ -1,8 +1,20 @@
 'use strict';
 
+require('./lib/baseObj');
+var hero = require('./lib/hero');
+var Barbarian = hero.Barbarian;
+var Wizard = hero.Wizard;
+require('./lib/board');
+
 var prompt = require('prompt');
-var classSchema = {
+var playerSchema = {
   properties: {
+    name: {
+      description: 'Please enter your character name',
+      pattern: /^[a-zA-Z0-9]+$/,
+      message: 'Letters and numbers only, please',
+      required: true
+    },
     class: {
       description: '(W)izard or (B)arbarian?',
       pattern: /^[BbWw]$/,
@@ -10,10 +22,21 @@ var classSchema = {
       required: true
     }
   }
-}
+};
 
+var player;
 
 prompt.start();
-prompt.get(classSchema, function(err, result) {
+prompt.get(playerSchema, function(err, result) {
+  console.log('name:  ' + result.name);
   console.log('class: ' + result.class);
+  if(err) {
+    console.error(err);
+  } else if (result.class === 'b' || result.class === 'B') {
+    player = new Barbarian(result.name);
+  } else if (result.class === 'w' || result.class === 'W') {
+    player = new Wizard(result.name);
+  } else {
+    console.log('bad input', result);
+  }
 });
